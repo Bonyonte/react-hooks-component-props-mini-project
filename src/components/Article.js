4f4343d0032c282f2d5bed7b8f67cff963d5cc2a
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-07-18 12:29:28 +0300

    Done.

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,24 +1,29 @@
 import React from 'react'
 
 function Article({title, date="January 1, 1970", preview, minutes}) {
-    const emojis = emojiList(minutes);
     
   return (
     <article>
         <h3>{title}</h3>
-        <small>{date} . {emojis} {minutes} min read</small>
+        <small>{date} | {emojiFromMinutes} {minutes} min read</small>
         <p>{preview}</p>
     </article>
   );
 }
 
-function emojiList (minutes){
+function emojiFromMinutes (minutes){
 
-    const interval = minutes < 30 ? 5 : 10;
+    const interval = 5;
 
-    const emoji = minutes < 30 ? "☕️" : "🍱";
-
+    const emoji = "☕️";
+    
     const emojis = "";
+    
+    if( minutes >= 30){
+      interval = 10;
+      emoji = "🍱";
+    }
+
     for (let i = 0; i < minutes; i+= interval){
         emojis += emoji;
     }
